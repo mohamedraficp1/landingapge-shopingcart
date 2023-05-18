c78f6d5c54d6fb92998343036f0ab4717956befe
mohamedraficp1 <rafi.acesoft@gmail.com>
2023-05-18 09:38:50 +0530

    API changed from dummy to node js backend

--- src/api/Config.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = "https://ill-plum-crab-robe.cyclic.app";
-const API_HEADER = {
-  "Content-Type": "application/json",
-  Accept: "application/json",
-};
-
-export { BASE_URL, API_HEADER };
